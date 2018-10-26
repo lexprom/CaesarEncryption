daddbd0b5abdc80dfec2e579107e97fbc54e3d75
Alexander Pak <pak@mercury.office>
2018-10-26 13:13:37 +0400

    initial commit

---
+++ Caesar Encryption/Caesar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Encryption
+{
+    class Caesar
+    {
+        static string alph = "абвгдеёжзийклмонпрстуфхцчшщъыьэюя";
+
+        public static string Encryption(string text)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (text[i] == alph[j])
+                        res.Append(alph[(j + 3) % alph.Length]);
+
+            return res.ToString();
+        }
+        public static string Decryption(string crypt)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < crypt.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (crypt[i] == alph[j])
+                        res.Append(alph[(j - 3 + alph.Length) % alph.Length]);
+
+            return res.ToString();
+        }
+    }
+}
---
+++ Caesar Encryption/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string text = "абвя";
+
+            string crypt = Caesar.Encryption(text);
+            string decrypt = Caesar.Decryption(crypt);
+
+            Console.WriteLine("Текст: {0}, Криптограмма: {1}, Дешифрофанный текст: {2}", text, crypt, decrypt);
+        }
+    }
+}
